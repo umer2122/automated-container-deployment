918214cae167f229b0a25506af5361e07d0153eb
umer2122 <33565517+umer2122@users.noreply.github.com>
2026-02-28 16:16:27 +0000

    Update SSH ingress rule to allow all IPs
    Changed SSH access to allow all IP addresses.

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_IP/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
