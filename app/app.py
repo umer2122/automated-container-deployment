4c305dfbfefca771bb997e2759a60d6e230d6070
umer2122 <33565517+umer2122@users.noreply.github.com>
2026-02-28 21:17:31 +0000

    Change app port from 5000 to 80

--- app/app.py
+++ app/app.py
@@ -6,4 +6,4 @@ def home():
     return "Networks & Systems Administration - Masters in Cybersecurity - Dublin Business School - Automated Cloud Deployment Successful!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
