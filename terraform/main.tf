9db3696b2dd36eadc266502a136bdef9fab3b6f5
umer2122 <33565517+umer2122@users.noreply.github.com>
2026-02-28 16:13:29 +0000

    Modify security group configuration and add random ID
    Updated security group name to include a random ID and restricted SSH access to a specific IP.

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,13 +3,14 @@ provider "aws" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name = "web_sg"
+  name        = "web_sg-${random_id.sg_id.hex}"
+  description = "Security group for web server"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["YOUR_IP/32"]
   }
 
   ingress {
@@ -25,4 +26,8 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "random_id" "sg_id" {
+  byte_length = 2
 }
