4b2a9f33dd6d588332fef3f788121c1d47444371
umer2122 <33565517+umer2122@users.noreply.github.com>
2026-02-28 21:08:08 +0000

    Change security group port from 22 to 5000

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,8 +7,8 @@ resource "aws_security_group" "web_sg" {
   description = "Security group for web server"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
